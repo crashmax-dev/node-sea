1678cf1187ccdc3734250049f6614d825d834950
Vitalij Ryndin <crashmax.dev@gmail.com>
2024-03-10 15:02:45 +0800

    feat: support `node@21.7.0`

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log(`Hello, ${process.argv[2] ?? 'World'}!`)
+const { getAsset } = require('node:sea')
+const { styleText } = require('node:util')
+const { userInfo } = require('node:os')
+
+const package = JSON.parse(getAsset('package.json', 'utf-8'))
+const username = styleText('bold', styleText('bgCyan', userInfo().username))
+const description = styleText('green', package.description)
+
+console.log(`Hello, ${username}!\n${description}`)
